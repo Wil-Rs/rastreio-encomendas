3c72cd8f9476c308456d3329e9eb99d994d4198d
Wilian Rodrigues <wilian.rodrigues@saudegarantida.com.br>
2021-09-07 21:49:55 -0300

    add bootstrap + cards

--- src/components/Eventos/index.jsx
+++ src/components/Eventos/index.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
+import 'bootstrap'
 import './index.css'
 
 export const Eventos = ({evObj, img}) => {
-
     return (
         <div className="eventos">
-            <div className="divImg">
-                <img className="img" src={img} alt="" />
+
+            <div class="card" >
+                <img class="card-img-top" src={img} alt="Imagem do status"/>
+                <div class="card-body">
+                    <h5 class="card-title">{evObj.descricao}</h5>
+                    <p class="card-text">Data: {evObj.data}</p>
+                    <p class="card-text">está: {evObj.unidade.local}</p>
+                    <p class="card-text">agencia: {evObj.unidade.tipounidade}</p>
+                    <p class="card-text">enviado para: { !!evObj.destino ? evObj.destino[0].cidade : 'Sem informação no momento'}</p>
+                </div>
             </div>
-            <div className="divInfos">
-                <p>Data: {evObj.data}</p>
-                <p>Descrição: {evObj.descricao}</p>
-                <p>está: {evObj.unidade.local} </p>
-                <p>agencia: {evObj.unidade.tipounidade}</p>
-            </div>
+
         </div>
     )
 }
